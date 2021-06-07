9384cf987347236ddf2fc4943b8f35fd97a0fefa
matias017 <73863103+matias017@users.noreply.github.com>
2021-06-07 19:42:37 -0300

    Update Cancion.js

--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -1,59 +1,49 @@
-import connection from './connection.js';
-import { ObjectId } from 'mongodb';
+const fs = require('fs').promises;
+const path= './data/browser.json';
 
-async function getCanciones() {
-    const clientMongo = await connection.getConnection();
-    console.log(clientMongo);
-    const canciones = await clientMongo.db('')
-        .collection('canciones')
-        .find()
-        .toArray();
-    return canciones;
+async function getCanciones(){
+    const canciones = await fs.readFile(path, 'utf-8');
+    return JSON.parse(canciones);
 }
 
-async function getCancion(id) {
-    const clientmongo = await connection.getConnection();
-    const cancion = await clientmongo.db('')
-        .collection('canciones')
-        .findOne({ _id: new ObjectId(id) });
+async function getCancion(id){
+    const canciones = await getCanciones();
+    return canciones.find(cancion => cancion._id == id);
+}
+
+async function addCancion(cancion){
+    const canciones = await getCanciones();
+    canciones.sort((a,b)=> a._id - b._id);
+    const lastId = canciones[canciones.length-1]._id;
+    cancion._id = lastId + 1;
+    canciones.push(cancion);
+    await fs.writeFile(path, JSON.stringify(canciones, null, ' '));
+
     return cancion;
 }
 
-async function addCancion(cancion) {
-    const clientmongo = await connection.getConnection();
-    const result = await clientmongo.db('')
-        .collection('canciones')
-        .insertOne(cancion);
-    return result;
+async function updateCancion(cancion){
+    const canciones = await getCanciones();
+    const index = canciones.findIndex(inv => inv._id == cancion._id);
+    if(cancion.first){
+        canciones[index].first = cancion.first;
+    }
+    if(cancion.last){
+        canciones[index].last = cancion.last;
+    }
+    if(cancion.year){
+        canciones[index].year = cancion.year;
+    }
+    await fs.writeFile(path, JSON.stringify(canciones, null, ' '));
+
+    return canciones[index];
 }
 
-async function updateCancion(cancion) {
-    const clientmongo = await connection.getConnection();
-    const query = { _id: new ObjectId(user._id) };
-    const newvalues = {
-        $set: {
-            nombre: user.nombre,
-            genero: user.genero,
-            dificultad: user.dificultad,
-            tiempo: user.tiempo,
-            imagen: user.imagen,
-            porcentaje:user.porcentaje,
-            popularidad:user.popularidad
-        }
-    };
-
-    const result = await clientmongo.db('')
-        .collection('canciones')
-        .updateOne(query, newvalues);
-    return result;
+async function deleteCancion(id){
+    const canciones = await getCanciones();
+    const index = canciones.findIndex(inv => inv._id == id);
+    canciones.splice(index,1);
+    await fs.writeFile(path, JSON.stringify(canciones, null, ' ')); 
 }
 
-async function deleteCancion(id) {
-    const clientmongo = await connection.getConnection();
-    const result = await clientmongo.db('')
-        .collection('cancion')
-        .deleteOne({ _id: new ObjectId(id) });
-    return result;
-}
-
-export default { getCancion, getCanciones, addCancion, updateCancion, deleteCancion };
+module.exports = {getCancion, getCanciones, addCancion, updateCancion, deleteCancion};
