d103d3429d3407aea0cef3fd7f0e2d6696327950
matias017 <73863103+matias017@users.noreply.github.com>
2021-06-07 19:44:02 -0300

    Create MenuUsuario.vue

---
+++ src/components/MenuUsuario.vue
@@ -0,0 +1,36 @@
+
+<template>
+<b-container fluid class="p-4 bg-dark">
+<div>
+    <div>  <router-link to ="/">home </router-link>   </div>
+    
+  <form>
+    <div>
+
+        <div id ="app-2">
+          <p1 v-bind ="menssage"> </p1>
+      
+        </div>
+    
+    </div>
+    
+  </form>
+    
+</div>
+
+</b-container>
+</template>
+
+<script>
+import cancion from '../components/cancion.js';
+  //var app2= new vue();
+    //app2
+    export default({
+      
+    }) ({
+  el: '#app-2',
+  data: {
+    message: 'su nivel es ' + cancion.getNivel()
+  }
+})   
+ </script>
